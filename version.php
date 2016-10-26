bf32e8adbcb1dbb5354bfdf535e934ae8e757244
valery <valery.fremaux@gmail.com>
2016-10-26 23:47:03 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -36,10 +36,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016040300; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2015111100; // Requires this Moodle version.
+$plugin->requires = 2016051900; // Requires this Moodle version.
 $plugin->component = 'auth_drupalservices'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.0.0 (Build 2016040300)';
+$plugin->release = '3.1.0 (Build 2016040300)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.0.0000';
+$plugin->codeincrement = '3.1.0000';
